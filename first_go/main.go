ed396bb01adb152fe429e623fc9a2f6cfb28b343
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-13 16:29:41 -0400

    adding var

--- first_go/main.go
+++ first_go/main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+// Declare variable outside a function
+// You need to use var keyword
+// It will automatically set to 0 if you don't assign the value.
+var z int
+var a = 3
+
 func main() {
+	fmt.Println("z = ", z)
 	x := 43
 	y := x + 4
 	fmt.Println(x)
+	fmt.Println(y)
 	fmt.Println("Hello World")
 	MyHello()
 }
