6120d14361d7cfa9a95e4d3460cf15d4d08b8cc4
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-16 13:40:38 -0400

    struct and embedded struct

---
+++ first_go/struct.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+	}
+
+	p2 := person{
+		first: "Miss",
+		last:  "Moneypenny",
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+
+	embeddedStruct()
+}
+
+func embeddedStruct() {
+	type secretAgent struct {
+		person
+		ltk bool
+	}
+
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+	// You can omit the person. The person is promoted inside the embedded structure.
+	// So you don't have to do sa1.person.first.
+	fmt.Println(sa1.first, sa1.person.last, sa1.ltk)
+
+	// But if you have a collision like in the example below
+	// first is defined inside person and also in the secretAgent2 struct.
+	type secretAgent2 struct {
+		person
+		first string
+		ltk   bool
+	}
+	sa2 := secretAgent2{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		first: "collision",
+		ltk:   true,
+	}
+	// Then you have to specify sa2.person.first to access the value of secretAgent.first.
+	fmt.Println(sa2.first, sa2.person.first, sa2.person.last, sa2.ltk)
+}
