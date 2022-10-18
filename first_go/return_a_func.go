798bc621e07afd579229926f141626beef7baa7e
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-17 22:00:16 -0400

    return a function

---
+++ first_go/return_a_func.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	s1 := foo()
+	fmt.Println(s1)
+
+	s2 := bar()
+	fmt.Printf("s2: %T\n", s2)
+	fmt.Println(s2())
+}
+
+func foo() string {
+	s := "Hello World"
+	return s
+}
+
+func bar() func() int {
+	return func() int {
+		return 2000
+	}
+}
