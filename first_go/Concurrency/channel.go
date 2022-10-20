a1f32d64ad4bbc5dbb5394e62c46e47e6ba9a51f
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-19 23:48:52 -0400

    add comment to channel.go

--- first_go/Concurrency/channel.go
+++ first_go/Concurrency/channel.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// When you have a function that returns a value,
+// you want to wrap it in another function that will become the go routine
+// Get the return value and put it in a channel
 func doSomething(x int) int {
 	return x * 5
 }
