710b9e5ad6516db2b0fe5fcaadb2fe7619355e76
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-20 12:01:13 -0400

    race condition

---
+++ first_go/Concurrency/race_condition.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// This is an example of racing condition
+// To find out if there is a race condition, run it with
+// go run -race race_condition.go
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("GoRoutine:", runtime.NumGoroutine())
+	// This will set the max num of CPU and return the previous setting
+	// fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(4))
+
+	counter := 0
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// This allows go to context switch, allowing other goroutines to run.
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("GoRoutine:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("GoRoutine:", runtime.NumGoroutine())
+	fmt.Println("Count:", counter)
+}
